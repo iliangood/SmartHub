e1050896ef13498e49be1933376311c9bf3a3033
iliangood <ilia191168@gmail.com>
2024-12-10 21:47:36 +0300

    Add a preset for the Telegram bot

---
+++ src/TgBot/TgBot.cpp
@@ -0,0 +1,14 @@
+#include <csignal>
+#include <cstdio>
+#include <cstdlib>
+#include <exception>
+#include <string>
+
+#include <tgbot/tgbot.h>
+
+using namespace std;
+using namespace TgBot;
+
+int main() {
+    return 0;
+}
