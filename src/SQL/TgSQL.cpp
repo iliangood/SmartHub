689963e110b64e799a78986cd8e0cae446cf8e23
iliangood <ilia191168@gmail.com>
2025-01-25 12:54:52 +0300

    add deleteUser function

--- src/SQL/TgSQL.cpp
+++ src/SQL/TgSQL.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 #define ADD_USER_MIN_PRIVILEGE 100
 #define MOD_USER_MIN_PRIVILEGE 100
+#define DELETE_USER_MIN_PRIVILEGE 100
 
 int getAddUserMinPrivilege()
 {
@@ -21,23 +22,28 @@ int getModUserMinPrivilege()
 	return MOD_USER_MIN_PRIVILEGE;
 }
 
-int userCount(sqlite3 * db, string userName, string *errString)
+int getDeleteUserMinPrivilege()
+{
+	return DELETE_USER_MIN_PRIVILEGE;
+}
+
+int userCount(sqlite3 * db, string userID, string *errString)
 {
 	sqlite3_stmt *res;
-	int rc = sqlite3_prepare_v2(db, "SELECT COUNT(*) FROM users WHERE name = ?", -1, &res, 0);
+	int rc = sqlite3_prepare_v2(db, "SELECT COUNT(*) FROM users WHERE userID = ?", -1, &res, 0);
 	if(rc != SQLITE_OK) //OK
 	{
 		const char *errmsg = sqlite3_errmsg(db);
-		Log(db, "userCount", userName, "", "FAIL_ERROR-SQLite:" + string(errmsg), errString);
+		Log(db, "userCount", userID, "", "FAIL_ERROR-SQLite:" + string(errmsg), errString);
 		errString->append("_userCount-FAIL_ERROR-SQLite:" + string(errmsg));
 		sqlite3_finalize(res);
 		return -1;
 	}
-	rc = sqlite3_bind_text(res, 1, userName.c_str(), -1, SQLITE_STATIC);
+	rc = sqlite3_bind_text(res, 1, userID.c_str(), -1, SQLITE_STATIC);
 	if(rc != SQLITE_OK) //OK
 	{
 		const char *errmsg = sqlite3_errmsg(db);
-		Log(db, "userCount", userName, "", "FAIL_ERROR-SQLite:" + string(errmsg), errString);
+		Log(db, "userCount", userID, "", "FAIL_ERROR-SQLite:" + string(errmsg), errString);
 		errString->append("_userCount-FAIL_ERROR-SQLite:" + string(errmsg));
 		sqlite3_finalize(res);
 		return -2;
@@ -45,14 +51,14 @@ int userCount(sqlite3 * db, string userName, string *errString)
 	rc = sqlite3_step(res);
 	if(rc == SQLITE_ROW) //OK
 	{
-		Log(db, "userCount", userName, "", "OK", errString);
+		Log(db, "userCount", userID, "", "OK", errString);
 		errString->append("_userCount-OK");
 		int result = sqlite3_column_int(res,0);
 		sqlite3_finalize(res);
 		return result;
 	}
 	const char *errmsg = sqlite3_errmsg(db); //TODO
-	Log(db, "userCount", userName, "", "FAIL_ERROR-SQLite:" + string(errmsg), errString);
+	Log(db, "userCount", userID, "", "FAIL_ERROR-SQLite:" + string(errmsg), errString);
 	errString->append("_userCount-FAIL_ERROR-SQLite:" + string(errmsg));
 	sqlite3_finalize(res);
 	return -3;
@@ -71,8 +77,8 @@ int getUserPrivilege(sqlite3 *db, string object, string *errString)
 	}
 	else if(count > 1) //OK
 	{
-		Log(db, "getUserPrivilege", object, "", "FAIL:there are a few users with that name", errString);
-		errString->append("_getUserPrivilege-FAIL:there are a few users with that name");
+		Log(db, "getUserPrivilege", object, "", "FAIL:there are a few users with that userID", errString);
+		errString->append("_getUserPrivilege-FAIL:there are a few users with that userID");
 		return -2;
 	}
 	else if(count < 0) //OK
@@ -81,7 +87,7 @@ int getUserPrivilege(sqlite3 *db, string object, string *errString)
 		errString->append("_getUserPrivilege-FAIL_ERROR-userCount:" + to_string(count));
 		return -3;
 	}
-	int rc = sqlite3_prepare_v2(db, "SELECT name, privilege FROM users WHERE name = ?", -1, &res, 0);
+	int rc = sqlite3_prepare_v2(db, "SELECT userID, privilege FROM users WHERE userID = ?", -1, &res, 0);
 	if(rc != SQLITE_OK) //OK
 	{
 		const char *errmsg = sqlite3_errmsg(db);
@@ -151,8 +157,8 @@ int modUser(sqlite3 *db, string object, string subject, int newPrivilege, string
 	}
 	if(count > 1) //OK
 	{
-		Log(db, "modUser", object, subject, "FAIL:there are a few users with that name", errString);
-		errString->append("_modUser-FAIL:there are a few users with that name");
+		Log(db, "modUser", object, subject, "FAIL:there are a few users with that userID", errString);
+		errString->append("_modUser-FAIL:there are a few users with that userID");
 		return -4;
 	}
 	int objectPrivilege = getUserPrivilege(db, object, errString);
@@ -169,7 +175,7 @@ int modUser(sqlite3 *db, string object, string subject, int newPrivilege, string
 		return -6;
 	}
 	sqlite3_stmt *res;
-	int rc = sqlite3_prepare_v2(db, "UPDATE users SET privilege = ? WHERE name = ?", -1, &res, 0);
+	int rc = sqlite3_prepare_v2(db, "UPDATE users SET privilege = ? WHERE userID = ?", -1, &res, 0);
 	if(rc != SQLITE_OK) //OK
 	{
 		const char *errmsg = sqlite3_errmsg(db);
@@ -219,8 +225,8 @@ int addUser(sqlite3 *db, string object, string subject, int privilege, string *e
 	}
 	if(countSubject > 1)
 	{
-		Log(db, "addUser", object, subject, "FAIL:there are a few users with that name", errString);
-		errString->append("_addUser-FAIL:there are a few users with that name");
+		Log(db, "addUser", object, subject, "FAIL:there are a few users with that userID", errString);
+		errString->append("_addUser-FAIL:there are a few users with that userID");
 		return -3;
 	}
 	int subjectPrivilege = getUserPrivilege(db, subject, errString);
@@ -239,8 +245,8 @@ int addUser(sqlite3 *db, string object, string subject, int privilege, string *e
 	int count = userCount(db, object, errString);
 	if(count > 0) //OK
 	{
-		Log(db, "addUser", object, subject, "FAIL:this username is already in use", errString);
-		errString->append("_addUser-FAIL:this username is already in use");
+		Log(db, "addUser", object, subject, "FAIL:this userID is already in use", errString);
+		errString->append("_addUser-FAIL:this userID is already in use");
 		return -6;
 	}
 	else if(count < 0) //OK
@@ -250,7 +256,7 @@ int addUser(sqlite3 *db, string object, string subject, int privilege, string *e
 		return -6;
 	}
 	sqlite3_stmt *res;
-	int rc = sqlite3_prepare_v2(db,"INSERT INTO users(name, privilege) VALUES(?, ?)", -1, &res, 0);
+	int rc = sqlite3_prepare_v2(db,"INSERT INTO users(userID, privilege) VALUES(?, ?)", -1, &res, 0);
 	if(rc != SQLITE_OK) //OK
 	{
 		const char *errmsg = sqlite3_errmsg(db);
@@ -283,8 +289,86 @@ int addUser(sqlite3 *db, string object, string subject, int privilege, string *e
 	return 0;
 }
 
+int deleteUser(sqlite3 *db, string object, string subject, string *errString)
+{
+	int countSubject = userCount(db, subject, errString);
+	if(countSubject < 0)
+	{
+		Log(db, "deleteUser", object, subject, "FAIL_ERROR-userCount:" + to_string(countSubject), errString);
+		errString->append("_deleteUser-FAIL_ERROR-userCount:" + to_string(countSubject));
+		return -1;
+	}
+	if(countSubject == 0)
+	{
+		Log(db, "deleteUser", object, subject, "FAIL:user is not exist", errString);
+		errString->append("_deleteUser-FAIL:user is not exist");
+		return -2;
+	}
+	if(countSubject > 1)
+	{
+		Log(db, "deleteUser", object, subject, "FAIL:there are a few users with that userID", errString);
+		errString->append("_deleteUser-FAIL:there are a few users with that userID");
+		return -3;
+	}
+	int countObject = userCount(db, object, errString);
+	if(countObject < 0)
+	{
+		Log(db, "deleteUser", object, subject, "FAIL_ERROR-userCount:" + to_string(countSubject), errString);
+		errString->append("_deleteUser-FAIL_ERROR-userCount:" + to_string(countSubject));
+		return -4;
+	}
+	if(countObject == 0)
+	{
+		Log(db, "deleteUser", object, subject, "FAIL:object is not exist", errString);
+		errString->append("_deleteUser-FAIL:object is not exist");
+		return -5;
+	}
+	int objectPrivilege = getUserPrivilege(db, object, errString);
+	if(objectPrivilege < 0) //OK
+	{
+		Log(db, "deleteUser", object, subject, "FAIL_ERROR-getUserPrivilege:" + to_string(objectPrivilege), errString);
+		errString->append("_deleteUser-FAIL_ERROR-userPrivilege:" + to_string(objectPrivilege));
+		return -6;
+	}
+	int subjectPrivilege = getUserPrivilege(db, subject, errString);
+	if(subjectPrivilege < 0) //OK
+	{
+		Log(db, "deleteUser", object, subject, "FAIL_ERROR-getUserPrivilege:" + to_string(subjectPrivilege), errString);
+		errString->append("_deleteUser-FAIL_ERROR-getUserPrivilege:" + to_string(subjectPrivilege));
+		return -7;
+	}
+	if(subjectPrivilege < getDeleteUserMinPrivilege() || subjectPrivilege < objectPrivilege) //OK
+	{
+		Log(db, "deleteUser", object, subject, "FAIL:the user does not have enough privileges", errString);
+		errString->append("_deleteUser-FAIL:the user does not have enough privileges");
+		return -6;
+	}
+	sqlite3_stmt *res;
+	int rc = sqlite3_prepare_v2(db,"DELETE FROM users WHERE userID = ?", -1, &res, 0);
+	if(!(sqlite3_bind_text(res, 1, object.c_str(), -1, SQLITE_STATIC) == SQLITE_OK))
+	{
+		const char *errmsg = sqlite3_errmsg(db);
+		Log(db, "deleteUser", object, subject, "FAIL_ERROR-SQLite:" + string(errmsg), errString); //OK
+		errString->append("_deleteUser-FAIL_ERROR-SQLite:" + string(errmsg));
+		sqlite3_finalize(res);
+		return -9;
+	}
+	sqlite3_step(res);
+	if(rc != SQLITE_OK)
+	{
+		const char *errmsg = sqlite3_errmsg(db);
+		Log(db, "deleteUser", object, subject, "FAIL_ERROR-SQLite:" + string(errmsg), errString); //OK
+		errString->append("_deleteUser-FAIL_ERROR-SQLite:" + string(errmsg));
+		sqlite3_finalize(res);
+		return -10;
+	}
+	Log(db, "deleteUser", object, subject, "OK", errString); //OK
+	errString->append("_deleteUser-OK");
+	sqlite3_finalize(res);
+	return 0;
+}
 
-tableInfo usersInfo("users", {column("id", "INTEGER"), column("name", "TEXT"), column("privilege", "INTEGER")});
+tableInfo usersInfo("users", {column("id", "INTEGER"), column("userID", "TEXT"), column("privilege", "INTEGER")});
 int initTgSQL(sqlite3 *db, string *errString)
 {
 	int rc = checkTable(db, usersInfo, errString);
@@ -323,11 +407,12 @@ int main()
 		sqlite3_close(db);
 		return 2;
 	}
-	string creator, username;
+	string creator, userID;
 	int privilege;
-	cin>>creator>>username>>privilege;
-	addUser(db, username ,creator, privilege, &errString);
+	cin>>creator>>userID;
+	rc = deleteUser(db, userID ,creator, &errString);
 	cout << errString << endl;
+	printf("code:%d\n", rc);
 	sqlite3_close(db);
 	return 0;
 }
